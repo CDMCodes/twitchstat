2e5ce54b53a2c56e5e2fb7b61cc4f79274be314d
Andrew Campbell <campbell.drew.m@gmail.com>
2017-08-15 21:35:11 -0400

    many updates to CSS styling, fixed js selector for add class

--- script.js
+++ script.js
@@ -5,26 +5,33 @@ $(document).ready(function(){
     qrystring = "https://wind-bow.gomix.me/twitch-api/streams/" + chanid + "?callback=?";
     $.getJSON(qrystring,function(result){
       console.log(result);
-      // console.log(this); //returns getJSON function
-      // console.log(elm); //returns passed in argument
       if(result.stream == null){
         $(elm).parent().append("<p>Sorry, not streaming right now.</p>");
+        $(elm).parent().addClass("offline");
         if($(elm).parent().hasClass("fcc")){ //special option for fcc button
           $(elm).parent().append("<p>Try one of these other channels</p>")
-          $("#others").slideDown("slow",function(){
+          $(".others").slideDown("slow",function(){
             //animation complete, do nothing
           })
         }
       }else{
-        $(elm).parent().append("<p>Here is the info!</p>");
+        $(elm).parent().addClass("streaming");
+        //better results is to add child div with collapse button,
+        // image, and stream details.  That way collapse button could hide it's
+        // parent div and still leave the initial button
+        $(elm).parent().append("<button class='collapse'>Collapse</button>");
+
+        //Nest image inside anchor linked to stream url
+        var imgadd = '<a href=' + result.stream.channel.url + ' target="_blank"><img src='+ result.stream.channel.logo + '></a>' ;
+        $(elm).parent().append(imgadd)
+
+        //loop through 4 keys named in this array and grab values for stream info
         var infofields = ["game","created_at","stream_type","viewers"];
-        // $(elm).parent().append("<p>Game: "+result.stream.game"</p>"+"<p>");
         var htmladd = "";
         infofields.forEach(function(val){
           htmladd +="<p>" + val + ": " + result.stream[val] + "</p>";
         });
-        var imgadd = '<a href=' + result.stream.channel.url + ' target="_blank"><img src='+ result.stream.channel.logo + '></a>' ;
-        $(elm).parent().append(imgadd)
+
         $(elm).parent().append(htmladd);
       }
     });
